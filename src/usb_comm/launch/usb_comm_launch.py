bb034240b505d16f7c108051d29c1499c213cc23
Anthony Kyung Guzman Leguel <kyung.guzman@gmail.com>
2023-11-19 20:13:59 +0900

    Added params and dev init to send info through usb

---
+++ src/usb_comm/launch/usb_comm_launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+
+
+def generate_launch_description():
+
+    config = os.path.join(
+      get_package_share_directory('usb_comm'),
+      'config',
+      'usb_comm_config.yaml'
+      )
+
+
+    return LaunchDescription([
+        Node(
+            package="usb_comm",
+            executable="usb_comm_node",
+            name="usb_comm",
+            output="screen",
+            emulate_tty=True,
+            parameters=[config]
+        )
+    ])
