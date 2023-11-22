27ef0959c720a23c991fa28d007793ddfffbe999
Anthony Kyung Guzman Leguel <kyung.guzman@gmail.com>
2023-11-22 23:42:57 +0900

    Fixed variable not found

--- src/usb_comm/src/serial_comm_node.cpp
+++ src/usb_comm/src/serial_comm_node.cpp
@@ -136,7 +136,7 @@ class USBSerialComm : public rclcpp::Node
     void readUSB_callback()
     {
     //   publisher_->publish(message);
-        read(fd, (void*)&dataFromCube_, sizeof(cubeDataRead_t));  
+        read(fd_, (void*)&dataFromCube_, sizeof(cubeDataRead_t));  
         std::cout << "recv message "<<std::endl;
         dataFromCube_.printStruct();
 
